5ad318134d6f5724a6a8341a54d45bfe05940c29
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-03-01 21:42:49 +0300

    Added script editing dialog buttons

--- app/windows/edit_script.py
+++ app/windows/edit_script.py
@@ -6,13 +6,58 @@ class EditScript(QtWidgets.QDialog):
     def __init__(self, path):
         super().__init__()
         self.setWindowTitle("Edit Script")
+        self.script = ScriptObj(path)
 
         self.main_layout = QtWidgets.QVBoxLayout(self)
         self.setLayout(self.main_layout)
         self.main_layout.setContentsMargins(20, 20, 20, 20)
         self.main_layout.setSpacing(10)
 
+        self._build_options()
+        self._build_buttons()
+
         self.adjustSize()
         self.setFixedSize(self.size())
 
-        self.script = ScriptObj(path)
+    def _build_options(self):
+        frame = QtWidgets.QFrame(frameShape=QtWidgets.QFrame.Shape.StyledPanel)
+        frame_layout = QtWidgets.QFormLayout(frame)
+        frame_layout.setSpacing(4)
+
+        self.name = QtWidgets.QLineEdit()
+        self.name.setText(self.script.name)
+        frame_layout.addRow("Script Name:", self.name)
+
+        self.keybind = QtWidgets.QKeySequenceEdit()
+        self.keybind.setKeySequence(self.script.keybind)
+        frame_layout.addRow("Keybind:", self.keybind)
+
+        self.main_layout.addWidget(frame)
+
+    def _build_buttons(self):
+        button_layout = QtWidgets.QVBoxLayout()
+        button_layout.setSpacing(1)
+
+        self.run_button = QtWidgets.QPushButton("Run")
+        self.run_button.clicked.connect(self._on_run)
+        button_layout.addWidget(self.run_button)
+
+        self.exit_button = QtWidgets.QPushButton("Exit")
+        self.exit_button.clicked.connect(self._on_exit)
+        button_layout.addWidget(self.exit_button)
+
+        self.delete_button = QtWidgets.QPushButton("Delete")
+        self.delete_button.setStyleSheet("color:red")
+        self.delete_button.clicked.connect(self._on_delete)
+        button_layout.addWidget(self.delete_button)
+
+        self.main_layout.addLayout(button_layout)
+
+    def _on_run(self):
+        pass
+
+    def _on_exit(self):
+        pass
+
+    def _on_delete(self):
+        pass
