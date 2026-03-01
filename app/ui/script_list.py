fc9a0775a15dd098a3833bf4c91cd13204a6c39a
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-03-01 19:19:45 +0300

    Fixed glitch where list buttons would run their function twice

--- app/ui/script_list.py
+++ app/ui/script_list.py
@@ -7,9 +7,10 @@ class ScriptList(QtWidgets.QListWidget):
     def __init__(self):
         super().__init__()
         self.add_button = None
+        self.itemClicked.connect(self._on_item)
         self.refresh_list()
 
-    #spamming this still duplicates script adding but whatever at least the program doesnt kill itself anymore
+    #duplication glitch GONE
     def _on_item(self, item):
         if item == self.add_button:
             add_script()
@@ -26,4 +27,3 @@ class ScriptList(QtWidgets.QListWidget):
 
         self.add_button = QtWidgets.QListWidgetItem("➕ Add Script")
         self.addItem(self.add_button)
-        self.itemClicked.connect(self._on_item)
