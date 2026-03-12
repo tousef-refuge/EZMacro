b6f3f3d1954cada96cf91347f066377ceb7ed9b5
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-03-12 20:57:51 +0300

    Instruction lists now give more info about their subscripts

--- app/ui/subscript.py
+++ app/ui/subscript.py
@@ -2,9 +2,17 @@ from PySide6 import QtWidgets
 
 from app.windows.edit_subscript import EditSubscript
 
+def subscript_name(instruct):
+    itype = instruct["type"].capitalize()
+    if itype == "Key":
+        info = instruct["key"]
+    else:
+        info = f"{instruct["x"]}, {instruct["y"]}"
+    return f"{itype} ({info})"
+
 class SubScript(QtWidgets.QListWidgetItem):
     def __init__(self, instruct, idx):
-        super().__init__(instruct["type"].capitalize())
+        super().__init__(subscript_name(instruct))
         self.instruct = instruct
         self.idx = idx
 
