8b92d9b2746a44c93117620c6c182ab68bf26e95
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-03-18 16:08:38 +0300

    Changed version to a Version object

--- app/version.py
+++ app/version.py
@@ -1,1 +1,25 @@
-VERSION = "1.2.1"
+#im sorry man the java brainrot got to me
+class Version:
+    def __init__(self, version):
+        self.version = version
+        self.parts = tuple(int(num) for num in version.split('.'))
+
+    def __lt__(self, other):
+        return self.parts < other.parts
+
+    def __le__(self, other):
+        return self.parts <= other.parts
+
+    def __gt__(self, other):
+        return self.parts > other.parts
+
+    def __ge__(self, other):
+        return self.parts >= other.parts
+
+    def __eq__(self, other):
+        return self.parts == other.parts
+
+    def __str__(self):
+        return self.version
+
+VERSION = Version("1.2.1")
