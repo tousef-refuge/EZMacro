8aea57a3a7ff1b9febc623c76c2de6e32ace5f22
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-03-18 15:53:20 +0300

    Added version.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from .version import VERSION
+
 PROJECT_DIR = Path(__file__).resolve().parent.parent
 SCRIPT_DIR = PROJECT_DIR / "scripts"
 SCRIPT_DIR.mkdir(parents=True, exist_ok=True)
