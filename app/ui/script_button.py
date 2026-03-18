b27dd83f34f10c72d875daa62523244b2f4b25d9
tousef-refuge <yoyo.alsh2007@gmail.com>
2026-03-18 16:32:07 +0300

    Script buttons now ask for confirmation if they have an outdated version attached

--- app/ui/script_button.py
+++ app/ui/script_button.py
@@ -1,12 +1,32 @@
-from PySide6 import QtWidgets
+from PySide6 import QtGui, QtWidgets
+from app import VERSION, VersionObj
 
 from app.windows.edit_script import EditScript #direct import cause circular imports SUCK
 
 class ScriptButton(QtWidgets.QListWidgetItem):
-    def __init__(self, script_obj):
+    def __init__(self, script_obj, parent):
         super().__init__(script_obj["name"])
+        self.parent = parent
         self.path = script_obj.path
 
+        self.version = VersionObj(script_obj["version"])
+        self.is_old = self.version < VERSION
+        if self.is_old:
+            self.setForeground(QtGui.QColor("red"))
+
     def open_window(self):
-        dialog = EditScript(self.path)
-        dialog.exec()
+        confirm = not self.is_old
+        if self.is_old:
+            dialog = QtWidgets.QMessageBox.question(
+                self.parent,
+                "Warning",
+                f"This script is on version {self.version} while\nyou are currently running version {VERSION}.\nRunning this script may crash the program.\nDo you want to open it?",
+                QtWidgets.QMessageBox.StandardButton.Yes |
+                QtWidgets.QMessageBox.StandardButton.No,
+                QtWidgets.QMessageBox.StandardButton.No
+            )
+            confirm = dialog == QtWidgets.QMessageBox.StandardButton.Yes
+
+        if confirm:
+            dialog = EditScript(self.path)
+            dialog.exec()
